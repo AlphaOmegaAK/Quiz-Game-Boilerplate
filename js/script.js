15a85e80c0ad183be284a9f4d1ea3f2a1c254aef
Artur <alphaunionxt@gmail.com>
2020-09-26 10:13:28 -0400

    Start Game Initiating

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const startBtn = document.getElementById('start-btn')
+startBtn.addEventListener('click', start)
+
+function start() {
+    console.log("Started");
+    startBtn.classList.add('hide')
+}
+
+function nextQues() {
+
+}
+
+function selAnswer() {
+
+}
