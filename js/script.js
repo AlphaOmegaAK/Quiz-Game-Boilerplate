ae3450041cdc739fea13dbe71c7cfdc04462fa49
Artur <alphaunionxt@gmail.com>
2020-09-26 10:15:35 -0400

    start and showing game board functional

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-const startBtn = document.getElementById('start-btn')
-startBtn.addEventListener('click', start)
+// ? Start Game + Start Button
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', start);
+
+// ? Questions
+const questionContEl = document.getElementById('question-container');
+
 
 function start() {
     console.log("Started");
-    startBtn.classList.add('hide')
-}
+    startBtn.classList.add('hide');
+
+    questionContEl.classList.remove('hide')
+};
 
 function nextQues() {
 
-}
+};
 
 function selAnswer() {
 
-}
+};
